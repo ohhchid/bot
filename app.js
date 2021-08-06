ed8daf63e13f0a8d3f36c2e0491240e46d075ded
ohhchid <88520434+ohhchid@users.noreply.github.com>
2021-08-05 22:19:29 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+const discord = require('discord.js');
+
+const bot = new discord.client();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.once('ready', () => {})
+console.log('yall bad but EEEEEEEE is online now')
+});
+
+
+
+client.login('ODczMDE1MDEwNTk3NjkxNDUy.YQyQlg.TKb50giRk5PaRubZ68GYKJo2wxc');
